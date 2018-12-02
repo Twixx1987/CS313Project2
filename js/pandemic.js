826cda78e4d6693dba33c05d8f67d2f1440dafb7
Shaun Densley <twixx1987@gmail.com>
2018-12-01 19:13:12 -0800

    working on initial home page

--- js/pandemic.js
+++ js/pandemic.js
@@ -25,7 +25,6 @@ function openJson(url, callbackFunc) {
 /*********************************************************************
 * This function generates the content of the home page
 **********************************************************************/
-// load the contents into home page
 function loadHome(xhttp) {
     // create an object and parse the JSON file
     let pandemicObj = JSON.parse(xhttp.responseText);
@@ -34,13 +33,27 @@ function loadHome(xhttp) {
     let length = pandemicObj.length;
 
     // create a variable to store the HTML for the display content
-    let text = "<table><tr><th>Version</th><th>Role</th><th>Abilities</th></tr>";
+    let text = "<h2>Pandemic the Game</h2>"
+             + "<p> As skilled members of a disease-fighting team, you "
+             + "and the other players work together to keep the world "
+             + "safe from outbreaks and epidemics. Only through teamwork "
+             + "will you have a chance to find a cure.</p><p>Pandemic is "
+             + "a cooperative board game in which players work as a team "
+             + "to treat infections around the world while gathering "
+             + "resources for cures. First published in 2007, the game's "
+             + "unique combination of cooperative gameplay, engrossing "
+             + "premise, and compelling design have proved a hit with "
+             + "everyone from hardcore gamers to casual players. The "
+             + "Pandemic game line now includes multiple expansions "
+             + "and stand-alone titles.</p><table><tr><th>Version</th>"
+             + "<th>Role</th><th>Abilities</th></tr>";
 
     // loop through the array object displaying each role
     for (let count = 0; count < length; count++) {
         // return a row with the role details to the text variable
-        text += "<tr><td>" + role.version + "</td><td>" + role.role
-             + "</td><td>" + role.abilities + "</td></tr>";
+        text += "<tr><td>" + pandemicObj[count].version + "</td><td>"
+			 + pandemicObj[count].role + "</td><td>"
+			 + pandemicObj[count].abilities + "</td></tr>";
     }
 
     // close the table
@@ -55,5 +68,32 @@ function loadHome(xhttp) {
 }
 
 /*************************************************************************
-*
+*This function generates the content of the statistics page
 ***************************************************************************/
+function loadPlayerRoleStats(xhttp) {
+    // create an object and parse the JSON file
+    let pandemicObj = JSON.parse(xhttp.responseText);
+
+    // get the length of the object array
+    let length = pandemicObj.length;
+
+    // create a variable to store the HTML for the display content
+    let text = "<table><tr><th>Role</th><th>Number of Plays</th></tr>";
+
+    // loop through the array object displaying each role
+    for (let count = 0; count < length; count++) {
+        // return a row with the role details to the text variable
+        text += "<tr><td>" + pandemicObj[count].role + "</td><td>"
+             + pandemicObj[count].play_count+ "</td></tr>";
+    }
+
+    // close the table
+    text += "</table>";
+
+    // set the table as the content of the mainContent div
+    let main = document.getElementById("mainContent");
+    main.innerHTML = text;
+
+    // return 
+    return;
+}
